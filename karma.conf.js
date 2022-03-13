1d0c451e63d6e063d92dbdb563f820a4a067a654
SHussainRR <45271087+SHussainRR@users.noreply.github.com>
2022-03-13 16:27:58 +0500

    configure project

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/projectX'),
+      dir: require('path').join(__dirname, './coverage/xyz-app'),
       subdir: '.',
       reporters: [
         { type: 'html' },
---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/homepage', pathMatch: 'full' },
+  {
+    path: 'homepage',
+    component: HomePageComponent,
+  },
+  { path: '**', redirectTo: '/homepage' },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
