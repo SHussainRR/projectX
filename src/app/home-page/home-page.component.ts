96311ece96782f3d70bf6b4e5639f22ab59812f2
Hussain Raza <hraza@vctech.io>
2022-03-13 17:02:12 +0500

    update configurations

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -8,5 +8,5 @@ import { Component, OnInit } from '@angular/core';
 export class HomePageComponent implements OnInit {
   constructor() {}
 
-  ngOnInit(): void {}
+                                          ngOnInit(): void {}
 }
